9642210843058623c41ba67ac440b14e381f15e3
lancealdwin <lancealdwin.s02@gmail.com>
2017-02-23 13:19:05 +0800

    1stspdrweb

---
+++ js/jslance.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
+	
+$(document).ready(function(){
+      $('.carousel').carousel();
+    });
+	
+	$(document).ready(function(){
+      $('.slider').slider({fullWidth: true});
+    });
+	$('.carousel.carousel-slider').carousel({fullWidth: true});
